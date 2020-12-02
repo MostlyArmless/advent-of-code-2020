eb6bb7b33c9375b5d7b7a0e22b4ee30e39f5651d
Mike Thiem <mike.j.thiem@gmail.com>
2020-12-01 19:29:06 -0700

    Added comparison tests for my different algorithm implementations

--- src/problem1.ts
+++ src/problem1.ts
@@ -12,10 +12,38 @@ export function problem1_part1( input: number[] ): number
 
 export function problem1_part2( input: number[] ): number
 {
-    return problem1_part2_bruteForce( input );
+    return problem1_part2_map( input );
 }
 
 export function problem1_part2_bruteForce( input: number[] ): number
+{
+    // Find {a,b,c} from elements in 'input' such that a+b+c = 2020. Return a*b*c.
+    for ( let i = 0; i < input.length; i++ )
+    {
+        const a = input[i];
+        for ( let j = 0; j < input.length; j++ )
+        {
+            if ( j === i )
+                continue; // Must use 3 different numbers
+
+            const b = input[j];
+            for ( let k = 0; k < input.length; k++ )
+            {
+                if ( k === i || k === j )
+                    continue; // Must use 3 different numbers
+
+                const c = input[k];
+
+                const sum = a + b + c;
+                if ( sum === 2020 )
+                    return a * b * c;
+
+            }
+        }
+    }
+}
+
+export function problem1_part2_map( input: number[] ): number
 {
     // Find {a,b,c} from elements in 'input' such that a+b+c = 2020. Return a*b*c.
     for ( let i = 0; i < input.length; i++ )
---
+++ test/performanceComparison.spec.ts
@@ -0,0 +1,20 @@
+import { expect } from "chai";
+import { measureExecutionTime } from "func-timer";
+import { PROBLEM_1_INPUT } from "../data/problemInputs";
+import { problem1_part2_bruteForce, problem1_part2_map } from "../src/problem1";
+
+const numRepetitions = 50;
+
+describe( 'Performance Comparisons', () =>
+{
+    it( 'Problem 1 Part 2 - Brute force VS map', async () =>
+    {
+        const r1 = await measureExecutionTime( problem1_part2_bruteForce, [PROBLEM_1_INPUT], numRepetitions );
+        const r2 = await measureExecutionTime( problem1_part2_map, [PROBLEM_1_INPUT], numRepetitions );
+
+        expect( r1.runtimeSeconds ).to.be.lte( 0.5 );
+        expect( r2.runtimeSeconds ).to.be.lte( 0.5 );
+        const fasterSlower = r1.runtimeSeconds < r2.runtimeSeconds ? "faster" : "slower";
+        console.log( `problem1_part2_bruteForce is ${fasterSlower} than problem1_part2_map` );
+    } );
+} );
